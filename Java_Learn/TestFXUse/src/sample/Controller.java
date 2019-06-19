70f56a49e5a537a0effa25aaedc4ead9656dcae3
AppleTree <978975474@qq.com>
2019-06-19 23:39:11 +0800

    use javaFX object

--- Java_Learn/TestFXUse/src/sample/Controller.java
+++ Java_Learn/TestFXUse/src/sample/Controller.java
@@ -1,11 +1,17 @@
 package sample;
 
+import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 
+import javafx.scene.control.TextArea;
 import java.net.URL;
 import java.util.ResourceBundle;
 
 public class Controller implements Initializable {
+
+    @FXML
+    private TextArea t_1;
+
     @Override
     public void initialize(URL location, ResourceBundle resources) {
 
@@ -13,5 +19,9 @@ public class Controller implements Initializable {
 
     public void Button_Click() {
         System.out.println("first");
+    }
+
+    public void right_Click(){
+        System.out.println(t_1.getAccessibleHelp());
     }
 }
