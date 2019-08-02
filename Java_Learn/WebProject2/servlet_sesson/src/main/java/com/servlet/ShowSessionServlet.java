57b2c6934d1358a39fa5d0bd7489128812eefdd2
AppleTree <978975474@qq.com>
2019-08-02 17:48:05 +0800

    some lack

---
+++ Java_Learn/WebProject2/servlet_sesson/src/main/java/com/servlet/ShowSessionServlet.java
@@ -0,0 +1,34 @@
+package com.servlet;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+/**
+ * @Classname ShowSessionServlet
+ * @Description TODO
+ * @Date 2019/7/27 20:07
+ * @Created by AppleTree
+ */
+public class ShowSessionServlet extends HttpServlet {
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        resp.setContentType("text/html;charset=UTF-8");
+        PrintWriter out = resp.getWriter();
+        //测试HttpSession
+        HttpSession httpSession = req.getSession();
+        String message = null;
+        Integer accessCount = (Integer)httpSession.getAttribute("accessCount");
+
+    }
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        this.doPost(req, resp);
+    }
+}
